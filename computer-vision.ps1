feecfd009c752c46fbb75e5ccc5db32519dbc812
Graeme Malcolm <gmalc@microsoft.com>
2021-07-22 15:19:45 -0700

    Added images

--- computer-vision.ps1
+++ computer-vision.ps1
@@ -2,33 +2,39 @@ $endpoint="https://gmalc-cog.cognitiveservices.azure.com/"
 $key="9fc9db061ed04f448cc31a01050bd976"
 
 
+
+# Code to call Computer Vision service for image analysis
+$img1 = "https://github.com/GraemeMalcolm/ai-stuff/raw/main/data/vision/store-cam1.jpg"
+
 $headers = @{}
 $headers.Add( "Ocp-Apim-Subscription-Key", $key )
 $headers.Add( "Content-Type","application/json" )
 
-$body = "{'documents' : [ `
-    {'id':1,'text':'Hello world.'}, `
-    {'id':2,'text':'Salut tout le monde.'} `
-    ]}"
+$body = "{'url' : '$img1'}"
 
-write-host "id text`n-- ----"
-$docs = $body | ConvertFrom-Json
-foreach ($doc in $docs.documents)
-{
-    write-host "$($doc.id) : $($doc.text)"
-}
-
+write-host "Analyzing image..."
 $result = Invoke-RestMethod -Method Post `
-          -Uri "$endpoint/text/analytics/v3.0/languages?" `
+          -Uri "$endpoint/vision/v3.2/analyze?visualFeatures=Categories,Description,Objects" `
           -Headers $headers `
           -Body $body | ConvertTo-Json -Depth 5
 
-
-$resultDocs = $result | ConvertFrom-Json
-foreach ($doc in $resultDocs.documents)
+$analysis = $result | ConvertFrom-Json
+Write-Host("`nDescription:")
+foreach ($caption in $analysis.description.captions)
 {
-    write-host($doc.id, $doc.detectedLanguage.name)
+    Write-Host ($caption.text)
 }
 
-#Full results
-($result | ConvertFrom-Json).documents
+Write-Host("`nObjects in this image:")
+foreach ($object in $analysis.objects)
+{
+    Write-Host (" -", $object.object)
+}
+
+Write-Host("`nTags relevant to this image:")
+foreach ($tag in $analysis.description.tags)
+{
+    Write-Host (" -", $tag)
+}
+
+Write-Host("`n")
