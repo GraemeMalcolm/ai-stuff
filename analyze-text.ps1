3487d3ebd1961db76b75c75b646d9e932f347422
Sherry <sheryang@microsoft.com>
2021-09-14 09:45:36 -0700

    Update lab 07 code.

--- analyze-text.ps1
+++ analyze-text.ps1
@@ -1,14 +1,14 @@
-$endpoint="YOUR_ENDPOINT"
-$key="YOUR_KEY"
+$endpoint="https://cog-serv-zi.cognitiveservices.azure.com/"
+$key="74ad078619614f4fa9dc10b4d8bcf3f6"
 
 # Code to call Text Analytics service to analyze sentiment in text
 $txt_file = "review1.txt"
-if ($args.count -gt 0 -And $args[0] -in ("review2.txt", "review3.txt", "review4.txt"))
-{
-    $txt_file = $args[0]
-}
+# if ($args.count -gt 0 -And $args[0] -in ("review2.txt", "review3.txt", "review4.txt"))
+# {
+#     $txt_file = $args[0]
+# }
 
-$txt = "https://github.com/GraemeMalcolm/ai-stuff/raw/main/data/text/reviews/$txt_file"
+$txt = "https://raw.githubusercontent.com/GraemeMalcolm/ai-stuff/main/data/text/reviews/$txt_file"
 
 $headers = @{}
 $headers.Add( "Ocp-Apim-Subscription-Key", $key )
@@ -16,18 +16,29 @@ $headers.Add( "Content-Type","application/json" )
 
 $body = "{'url' : '$txt'}"
 
+Write-Host("`n $body")
+
 write-host "Analyzing text...`n"
 $result = Invoke-RestMethod -Method Post `
-          -Uri "$endpoint/text/analytics/v3.1/languages" `
+          -Uri "$endpoint/text/analytics/v3.1/keyPhrases" `
           -Headers $headers `
           -Body $body | ConvertTo-Json -Depth 6
 
-$analysis = ($result | ConvertFrom-Json)
+# $result2 = Invoke-RestMethod -Method Post `
+#           -Uri "$endpoint/text/analytics/v3.1/keyPhrases" `
+#           -Headers $headers `
+#           -Body $body | ConvertTo-Json -Depth 6
 
-# foreach ($listofdict in $analysis.regions.lines.words)
-# {
-#     foreach($dict in $listofdict)
-#     {
-#         Write-Host ("$dict, `n")
-#     }
-# }
+$analysis = ($result | ConvertFrom-Json)
+#$analysis2 = ($result2 | ConvertFrom-Json)
+# $analysis2.documents[0].keyPhrases,
+
+
+Write-Host("`nLanguage Detected:")
+foreach ($language in $analysis.documents)
+{
+    Write-Host ($language.detectedLanguage)
+}
+
+
+
