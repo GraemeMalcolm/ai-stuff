95126f248501f389a0d80280e7886d3e101fe841
Sarah Guthals <sarah@guthals.com>
2021-09-15 11:34:31 -0700

    Working form-recognizer

--- form-recognizer.ps1
+++ form-recognizer.ps1
@@ -1,11 +1,8 @@
-$endpoint="ENDPOINT"
-$key="KEY"
-
-# For this unit, there is only one sample receipt image
-$img_file = "receipt.jpg"
+$endpoint="YOUR_ENDPOINT"
+$key="YOUR_KEY"
 
 # Create the URL where the raw receipt image can be found
-$img = "https://github.com/GraemeMalcolm/ai-stuff/raw/main/data/vision/$img_file"
+$img = "https://raw.githubusercontent.com/GraemeMalcolm/ai-stuff/main/data/vision/receipt.jpg"
 
 # Create the header for the REST POST with the subscription key
 # In this example, the URL of the image will be sent instead of 
@@ -15,35 +12,57 @@ $headers.Add( "Ocp-Apim-Subscription-Key", $key )
 $headers.Add( "Content-Type","application/json" )
 
 # Create the body with the URL of the raw image
-$body = "{ 'source': '$img' }"
+$body = "{'source': '$img'}"
 
 # Call the receipt analyze method with the header and body
-Write-Host "Analyzing receipt..."
-$result = Invoke-RestMethod -Method Post `
-          -Uri "$endpoint/formrecognizer/v2.1-preview.2/prebuilt/receipt/analyze" `
+# Must call the Invoke-WebRequest to have acces to the header
+Write-Host "Sending receipt..."
+$response = Invoke-WebRequest -Method Post `
+          -Uri "$endpoint/formrecognizer/v2.1/prebuilt/receipt/analyze" `
           -Headers $headers `
-          -Body $body | ConvertTo-Json -Depth 6 
+          -Body $body
+Write-Host "...Receipt sent."
 
-$analysis = $result | ConvertFrom-Json
-#Write-Host "type: $($analysis.GetType())"
-#Write-Host "resultID : $($analysis.headers["operation-location"])`n"
+# Extract the URL from the response of the receipt anaylzer 
+# to call the API to getting the analysis results
+$resultUrl = $($response.Headers['Operation-Location'])
 
-# Extract the resultID from the response of the receipt anaylzer
+# Create the header for the REST GET with only the subscription key
+$resultHeaders = @{}
+$resultHeaders.Add( "Ocp-Apim-Subscription-Key", $key )
 
+# Get the receipt analysis results, passing in the resultURL
+# Continue to request results until the analysis is "succeeded"
+Write-Host "Getting results..."
+Do {
+    $result = Invoke-RestMethod -Method Get `
+            -Uri $resultUrl `
+            -Headers $resultHeaders | ConvertTo-Json -Depth 10
 
-# Create the header for the REST POST with the subscription key
-# $resultHeaders = @{}
-# $resultHeaders.Add( "Ocp-Apim-Subscription-Key", $key )
+    $analysis = ($result | ConvertFrom-Json)
+} while ($analysis.status -ne "succeeded")
+Write-Host "...Done`n"
 
-# # Get the receipt analysis results, passing in the resultID
-# Write-Host "Getting results..."
-# $result = Invoke-RestMethod -Method Get `
-#           -Uri "$endpoint/formrecognizer/v2.1/prebuilt/receipt/analyzeResults/$resultID" `
-#           -Headers $resultHeaders | ConvertTo-Json -Depth 10
-# Write-Host "...Done`n"
-
-# # Get the object of the receipt analysis results
-# $analysis = ($result | ConvertFrom-Json)
+# Access the relevant fields from the analysis 
+$analysisFields = $analysis.analyzeResult.documentResults.fields
 
 # Print out all of the properties of the receipt analysis
-# TODO
+Write-Host ("Receipt Type: ", $($analysisFields.fields.ReceiptType.valueString))
+Write-Host ("Merchant Address: ", $($analysisFields.fields.MerchantAddress.valueString))
+Write-Host ("Merchant Phone: ", $($analysisFields.MerchantPhoneNumber.valueString))
+Write-Host ("Transaction Date: ", $($analysisFields.TransactionDate.valueString))
+Write-Host ("Receipt Items: ")
+
+# Access the individual items from the analysis
+$receiptItems = $($analysisFields.fields.Items.valueArray)
+
+for (($idx = 0); $idx -lt $receiptItems.Length; $idx++) {
+    $item = $receiptItems[$idx] 
+    Write-Host ("Item #", ($idx+1))
+    Write-Host ("  - Name: ", $($item.valueObject.Name.valueString))
+    Write-Host ("  - Price: ",$($item.valueObject.TotalPrice.valueNumber))
+}
+
+Write-Host ("Subtotal: ", $($analysisFields.Subtotal.valueNumber))
+Write-Host ("Tax: ", $($analysisFields.Tax.valueNumber))
+Write-Host ("Total: ", $($analysisFields.Total.valueNumber))
