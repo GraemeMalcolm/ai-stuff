33c80cd5b03d06c4efc18b0d6298b4514af0e59e
Sherry <sheryang@microsoft.com>
2021-09-14 21:41:01 -0700

    Update lab 06 code.

--- form-recognizer.ps1
+++ form-recognizer.ps1
@@ -1,13 +1,11 @@
-$endpoint="YOUR_ENDPOINT"
-$key="YOUR_KEY"
+$endpoint="ENDPOINT"
+$key="KEY"
 
 # For this unit, there is only one sample receipt image
 $img_file = "receipt.jpg"
 
 # Create the URL where the raw receipt image can be found
-$img = "https://raw.githubusercontent.com/GraemeMalcolm/ai-stuff/main/data/vision/$img_file"
-
-Write-Host $img
+$img = "https://github.com/GraemeMalcolm/ai-stuff/raw/main/data/vision/$img_file"
 
 # Create the header for the REST POST with the subscription key
 # In this example, the URL of the image will be sent instead of 
@@ -22,28 +20,30 @@ $body = "{ 'source': '$img' }"
 # Call the receipt analyze method with the header and body
 Write-Host "Analyzing receipt..."
 $result = Invoke-RestMethod -Method Post `
-          -Uri "$endpoint/formrecognizer/v2.1/prebuilt/receipt/analyze?includeTextDetails=true&locale=en-US" `
+          -Uri "$endpoint/formrecognizer/v2.1-preview.2/prebuilt/receipt/analyze" `
           -Headers $headers `
-          -Body $body | ConvertTo-Json -Depth 10
-Write-Host "...Done`n"
+          -Body $body | ConvertTo-Json -Depth 6 
+
+$analysis = $result | ConvertFrom-Json
+#Write-Host "type: $($analysis.GetType())"
+#Write-Host "resultID : $($analysis.headers["operation-location"])`n"
 
 # Extract the resultID from the response of the receipt anaylzer
-# TODO
-$resultID = 0
+
 
 # Create the header for the REST POST with the subscription key
-$resultHeaders = @{}
-$resultHeaders.Add( "Ocp-Apim-Subscription-Key", $key )
+# $resultHeaders = @{}
+# $resultHeaders.Add( "Ocp-Apim-Subscription-Key", $key )
 
-# Get the receipt analysis results, passing in the resultID
-Write-Host "Getting results..."
-$result = Invoke-RestMethod -Method Get `
-          -Uri "$endpoint/formrecognizer/v2.1/prebuilt/receipt/analyzeResults/$resultID" `
-          -Headers $resultHeaders | ConvertTo-Json -Depth 10
-Write-Host "...Done`n"
+# # Get the receipt analysis results, passing in the resultID
+# Write-Host "Getting results..."
+# $result = Invoke-RestMethod -Method Get `
+#           -Uri "$endpoint/formrecognizer/v2.1/prebuilt/receipt/analyzeResults/$resultID" `
+#           -Headers $resultHeaders | ConvertTo-Json -Depth 10
+# Write-Host "...Done`n"
 
-# Get the object of the receipt analysis results
-$analysis = ($result | ConvertFrom-Json)
+# # Get the object of the receipt analysis results
+# $analysis = ($result | ConvertFrom-Json)
 
 # Print out all of the properties of the receipt analysis
 # TODO
